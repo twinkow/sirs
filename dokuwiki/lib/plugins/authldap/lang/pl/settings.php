f598f045361d79a81d74e5841401b53a14669e66
Alex <alexcp.almeida@gmail.com>
2013-11-13 23:18:27 +0000

    Removed

--- dokuwiki/lib/plugins/authldap/lang/pl/settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Polish language file
- *
- */
-$lang['starttls']              = 'Użyć połączeń TLS?';
-$lang['bindpw']                = 'Hasło powyższego użytkownika';
--- dokuwiki/lib/plugins/contentencryption/conf/default.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Default settings for the contentencryption plugin
- *
- * @author Alex <alexcp.almeida@gmail.com>
- */
-
-//$conf['fixme']    = 'FIXME';
