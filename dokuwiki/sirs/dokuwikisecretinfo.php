11e89cb1a3671b66087533bfaf18f202c5de65cb
Alex <alexcp.almeida@gmail.com>
2013-11-28 11:52:56 +0000

    Key generation a bit improved

--- dokuwiki/sirs/dokuwikisecretinfo.php
+++ dokuwiki/sirs/dokuwikisecretinfo.php
@@ -7,11 +7,7 @@ class DokuWikiSecretInfo {
     
     static function generateDokuWikiKey(){
 
-        $data = array(
-            'password' => '',
-            'foruser'  => 'DokuWikiManager'
-        );
-
+        $password = '';
         $c = 'bcdfghjklmnprstvwz'; //consonants except hard to speak ones
         $v = 'aeiou'; //vowels
         $a = $c.$v; //both
@@ -19,14 +15,15 @@ class DokuWikiSecretInfo {
 
         //use thre syllables...
         for($i = 0; $i < 64; $i++) {
-            $data['password'] .= $c[auth_random(0, strlen($c) - 1)];
-            $data['password'] .= $v[auth_random(0, strlen($v) - 1)];
-            $data['password'] .= $a[auth_random(0, strlen($a) - 1)];
+            $password .= $c[auth_random(0, strlen($c) - 1)];
+            $password .= $v[auth_random(0, strlen($v) - 1)];
+            $password .= $s[auth_random(0, strlen($s) - 1)];
+            $password .= $a[auth_random(0, strlen($a) - 1)];
         }
         //... and add a nice number and special
-        $data['password'] .= auth_random(10, 99).$s[auth_random(0, strlen($s) - 1)];
+        $password .= auth_random(10, 99).$s[auth_random(0, strlen($s) - 1)];
 
-        return $data['password'];
+        return $password;
     }
 
     static function storeDokuWikiKey($key){
@@ -39,8 +36,7 @@ class DokuWikiSecretInfo {
         $file = 'sirs/securelocation/sirs.txt';
         $text = file_get_contents($file);
         $atKey = strstr($text, '@');
-        $key = explode('@', $atKey);
-        return $key[1];
+        return explode('@', $atKey)[1];
     }
 }
 
