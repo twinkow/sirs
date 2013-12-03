97925ff71f5c78b32028207a799ad8ec01b0160b
João Ferreira Loff <jfloff@gmail.com>
2013-12-03 23:07:58 +0000

    file submit

--- dokuwiki/lib/plugins/digitalsignature/script.js
+++ dokuwiki/lib/plugins/digitalsignature/script.js
@@ -1,14 +1,14 @@
 /**
  * With the first function we create a new button type called Click
- * 
+ *
  * the function name must be addBtnAction<Your type name>
  * in our case it is addBtnActionClick
  *
  * in the other function we simply use the simple toolbar method with the new type
- * 
+ *
  * you can easily extend it to complex scripts like the link wizard etc
  */
- 
+
 /**
  * Add button action for your toolbar button
  *
@@ -18,10 +18,13 @@
  * @return {boolean}  If button should be appended
  */
 function addBtnActionDigsig($btn, props, edid) {
+
+    jQuery("body").append("<input type='file' style='display: none;' name='file' id='file'>");
+
     $btn.click(function() {
-        
+        jQuery('#file').trigger('click');
         return false;
     });
- 
+
     return true;
 }
